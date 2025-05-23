7c44cc8ca20a3df4bee199a81b0963f6e44d9ab6
Trinh Minh Triet <contact@trinhminhtriet.com>
2025-05-23 21:44:13 +0700

    refactor: restructure main function to support dynamic function calls via command line arguments

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -29,9 +30,30 @@ def github_collect_repo():
 def hellogithub_collect_repo():
     from controllers.hellogithub_ctl import HelloGitHubController
 
+
     ctl = HelloGitHubController()
     ctl.collect_repo(max_pages=1000)
 
 
 if __name__ == "__main__":
-    hellogithub_collect_repo()
+    
+    funcs = {
+        "hellogithub_collect_repo": hellogithub_collect_repo,
+        "gitstar_collect_repo": gitstar_collect_repo,
+        "gitstar_collect_user": gitstar_collect_user,
+        "github_collect_repo": github_collect_repo,
+    }
+
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <function_name>")
+        print("Available functions:", ", ".join(funcs.keys()))
+        sys.exit(1)
+
+    func_name = sys.argv[1]
+    func = funcs.get(func_name)
+    if func is None:
+        print(f"Unknown function: {func_name}")
+        print("Available functions:", ", ".join(funcs.keys()))
+        sys.exit(1)
+
+    func()
